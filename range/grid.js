431fcf26e7119c62590ef8a783d12514bf1d6b26
Jean-Baptiste Courtois <jean-baptiste.courtois@sap.com>
2023-07-17 17:27:10 +0200

    Tokens + combos

--- range/grid.js
+++ range/grid.js
@@ -7,6 +7,9 @@ const ACTIONS = [
 	{ "id": "Fold", "color": "blue" },
 ];
 
+var combos = {};
+var suitedness = new Set(["ss", "hh", "dd", "cc"]);
+
 function generateCell(name) {
 	let cell = document.createElement("td");
 	cell.id = name;
@@ -21,18 +24,80 @@ function generateCell(name) {
 	square.appendChild(content);
 
 	// Areas
-	for (let action of ACTIONS) {
+	for (let index = 0; index < ACTIONS.length; index++) {
+		let action = ACTIONS[index];
 		let area = document.createElement("div");
-		area.id = "grid-" + name + "-" + action.id;
+		area.id = "grid-" + name + "-" + index;
 		area.style.backgroundColor = action.color;
-		area.style.flexBasis = "21%";
+		area.style.flexBasis = "0%";
 		square.appendChild(area);
 	}
 
 	return cell;
 }
 
-function refreshGrid(grid) {
+/**
+ * Convert a combo to its generic token.
+ * Use global variable suitedness for the suffix, even for pairs.
+ *
+ * Examples:
+ * getToken("JhTh"); // JTs
+ * getToken("TsJh"); // JTo
+ * getToken("8s8h"); // 88o
+ */
+function getToken(combo) {
+	var rank1 = combo[0];
+	var rank2 = combo[2];
+	var suit1 = combo[1];
+	var suit2 = combo[3];
+
+	if (RANKS.indexOf(rank1) > RANKS.indexOf(rank2)) {
+		// Put the highest card first
+		[rank1, rank2] = [rank2, rank1];
+		[suit1, suit2] = [suit2, suit1];
+	}
+
+	const suited = (suitedness.has(suit1 + suit2)) ? "s" : "o";
+	return rank1 + rank2 + suited;
+}
+
+function getAllTokens() {
+	const tokens = {};
+
+	// Regroup weights
+	for (let [combo, weights] of Object.entries(combos)) {
+		const token = getToken(combo);
+		const weightLists = tokens[token] || [];
+		for (let i = 0; i < weights.length; i++) {
+			weightLists[i] = weightLists[i] || [];
+			weightLists[i].push(weights[i]);
+		}
+		tokens[token] = weightLists;
+	}
+
+	// Compute averages
+	for (let [token, weights] of Object.entries(tokens)) {
+		tokens[token] = weights.map((weightList) => {
+			const sum = weightList.reduce((acc, weight) => acc + weight, 0);
+			return sum / weightList.length;
+		});
+	}
+
+	return tokens;
+}
+
+function refreshCombos() {
+	// Update grid
+	for (let [token, weights] of Object.entries(getAllTokens())) {
+		for (let index = 0; index < weights.length; index++) {
+			const weight = weights[index];
+			const area = document.getElementById("grid-" + token + "-" + index);
+			area.style.flexBasis = String(weight * 100) + "%";
+		}
+	}
+}
+
+function refreshGrid() {
 	grid.innerHTML = "";
 	for (let i1 = 0; i1 < RANKS.length; i1++) {
 		let r1 = RANKS[i1];
@@ -42,17 +107,17 @@ function refreshGrid(grid) {
 
 			if (i1 > i2) {
 				row.appendChild(generateCell(r2 + r1 + "o"));
-			}
-			else if (i1 < i2) {
+			} else if (i1 < i2) {
 				row.appendChild(generateCell(r1 + r2 + "s"));
-			}
-			else {
+			} else {
 				row.appendChild(generateCell(r1 + r2 + "o"));
 				row.appendChild(generateCell(r1 + r2 + "s"));
 			}
 		}
 		grid.appendChild(row);
 	}
+
+	refreshCombos();
 }
 
-refreshGrid(grid);
+refreshGrid();
